6c590879e09967101501c45001481874b94f3f6f
Amanda <85398936+Sanduamanda99@users.noreply.github.com>
2024-12-16 07:33:06 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        customPurple: "#9966CC",
+        customIndigo: "#6A5ACD",
+        customDarkerIndigo: "#4B0082",
+        customMagenta: "#9400D3",
+        customLavender: "#E6E6FA",
+        customThistle: "#D8BFD8",
+      },
+    },
+  },
+  plugins: [],
+};
